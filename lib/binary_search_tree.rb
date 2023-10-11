cb598695daf81f61064d2f01d92c9314710d2198
sjamescarter <stephenjamescarter@gmail.com>
2023-10-11 12:52:45 -0500

    commit tree a80636dce7195ce7f85eedb413da9b2c981cf82c

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,47 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    current_node = @root
+    
+    while current_node != nil
+      if current_node.value == value
+        return current_node
+      elsif current_node.value > value
+        current_node = current_node.left
+      else
+        current_node = current_node.right
+      end
+    end
+
+    nil
   end
 
   def insert(value)
-    # your code here
-  end
+    node = Node.new(value)
+    current_node = @root
+    
+    if current_node.nil?
+      @root = node
+    else
+      while current_node != nil
+        if current_node.value == value
+          return current_node
+        elsif current_node.value > value
+          if current_node.left.nil?
+            current_node.left = node
+          else
+            current_node = current_node.left
+          end
+        else
+          if current_node.right.nil?
+            current_node.right = node
+          else
+            current_node = current_node.right
+          end
+        end
+      end
+    end
 
+    node
+  end
 end 
